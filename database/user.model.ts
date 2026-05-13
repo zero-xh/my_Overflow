4ab1137fac8d70fac33c4616864867e4fd4ce359
zero-xh <93333954+zero-xh@users.noreply.github.com>
2026-05-13 09:35:09 +0800

    创建用户model

---
+++ database/user.model.ts
@@ -0,0 +1,31 @@
+import { model, models, Schema } from "mongoose"
+
+export interface IUser {
+    name: string
+    username: string
+    email: string
+    bio?: string
+    image: string
+    location?: string
+    protfolio?: string
+    reputation?: string
+}
+
+const UserSchema = new Schema(
+    {
+        name: { type: String, require: true },
+        username: { type: String, require: true },
+        email: { type: String, require: true, unique: true },
+        bio: { type: String },
+        image: { type: String, require: true },
+        location: { type: String },
+        protfolio: { type: String },
+        reputation: { type: Number, require: 0 },
+    },
+    {
+        timestamps: true
+    }
+)
+
+const User = models?.user || model<IUser>("User", UserSchema);
+export default User
